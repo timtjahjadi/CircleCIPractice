0a57901510a208839193a8f8169021641f35eac9
Timotius Tjahjadi - DKI <timotius.tjahjadi@danakini.co.id>
2022-05-11 14:35:21 +0700

    set config and unit test

--- CircleCIPracticeTests/CircleCIPracticeTests.swift
+++ CircleCIPracticeTests/CircleCIPracticeTests.swift
@@ -29,11 +29,4 @@ class CircleCIPracticeTests: XCTestCase {
         XCTAssert(i == 0, "i supposed to be 0")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
