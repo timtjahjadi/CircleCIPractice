8244f8b942cdd71235847fa87277e78eab097127
Timotius Tjahjadi - DKI <timotius.tjahjadi@danakini.co.id>
2022-05-11 13:51:50 +0700

    Initial Commit

---
+++ CircleCIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircleCIPractice
+//
+//  Created by 163611 TIMOTIUS on 5/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
